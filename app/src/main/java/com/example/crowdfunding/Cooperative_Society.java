11185feaf99540cd90725e3cceb517c36ed7eff8
HP <aishwaryamurali11@gmail.com>
2020-05-18 15:52:28 +0530

    Integrated part

--- app/src/main/java/com/example/crowdfunding/Cooperative_Society.java
+++ app/src/main/java/com/example/crowdfunding/Cooperative_Society.java
@@ -3,12 +3,29 @@ package com.example.crowdfunding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Cooperative_Society extends AppCompatActivity {
-
+    Button scheme_button,insurance_button,farmer_details,crop_details,register_farmer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cooperative_society);
+        scheme_button=findViewById(R.id.button_scheme);
+        insurance_button=findViewById(R.id.insurance_button);
+        farmer_details=findViewById(R.id.farmer_button);
+        crop_details=findViewById(R.id.crop_details);
+        register_farmer=findViewById(R.id.crop_details);
+
+
+        scheme_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+    
+
 }
---
+++ app/src/main/java/com/example/crowdfunding/Cooperative_schemes.java
@@ -0,0 +1,14 @@
+package com.example.crowdfunding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cooperative_schemes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cooperative_schemes);
+    }
+}
